5e6a891666f17c8ce05cf762cb3a185648abf50b
kathu1207 <wnsdn69@naver.com>
2017-03-06 16:54:42 +0900

    곁치지않는 라인

--- 17_03_06/17_03_06/Program.cs
+++ 17_03_06/17_03_06/Program.cs
@@ -10,6 +10,28 @@ namespace _17_03_06
     {
         static void Main(string[] args)
         {
+            int start_1 = 1;
+            int end_1 = 3;
+
+            int start_2 = -1;
+            int end_2 = 2;
+
+            if(start_1 > end_1)
+            {
+                int box = start_1;
+                start_1 = end_1;
+                end_1 = box;        
+            }
+
+            if((start_1 < end_2) && (end_1 > start_2))
+            {
+                Console.WriteLine("라인이 겹친다.");
+            }
+            else
+            {
+                Console.WriteLine("라인이 겹치지않는다.");
+            }
+  
         }
     }
 }
