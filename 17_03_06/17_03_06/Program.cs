405c051b22c9a02b42b7d073adbd09091a9dbec8
kathu1207 <wnsdn69@naver.com>
2017-03-07 17:13:03 +0900

    공부

--- 17_03_06/17_03_06/Program.cs
+++ 17_03_06/17_03_06/Program.cs
@@ -10,24 +10,30 @@ namespace _17_03_06
     {
         static void Main(string[] args)
         {
-            int start_1 = 1;
-            int end_1 = 3;
+            int start_1 = -1;
+            int end_1 = -2;
 
             int start_2 = -1;
-            int end_2 = 2;
-
+            int end_2 = -3;
+             
             if(start_1 > end_1)
             {
                 int box = start_1;
                 start_1 = end_1;
                 end_1 = box;        
             }
+            else if(start_2 > end_2)
+            {
+                int box = start_2;
+                start_1 = end_2;
+                end_2 = box;
+            }
 
-            if((start_1 < start_2) && (end_1 < start_2))
+            if ((start_1 < start_2) && (end_1 < start_2))
             {
                 Console.WriteLine("라인이 겹친다.");
             }
-            else if((start_2 < start_1) && (end_2 < start_1)
+            else if((start_2 < start_1) && (end_2 > start_1))
             {
                 Console.WriteLine("라인이 겹친다.");
             }
