8c7731b104a40d00bedb436f9d19a854d52080db
kathu1207 <wnsdn69@naver.com>
2017-03-08 17:36:43 +0900

    토너머트(미완성)

---
+++ 17_03_08(6)/17_03_08(6)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_03_08_6_
+{
+    // 분할 하고 분할해서 마지막에 개별로 만든 다음에 큰수 끼리 비교..(토너먼트)
+    class Program
+    {
+        public static void division(int[] arr)// 반을 쪼개는 작업
+        {
+            int[] save = new int[arr.Length];
+            if(save[])
+            for(int i= 0; i<arr.Length; i++)
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] score = new int[20]{
+                80,74,81,90,34,84,76,95,45,66,74,82,76,57,51,88,73,98,51,60};
+
+        }
+    }
+}
