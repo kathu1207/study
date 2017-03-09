f5247e700adde5cdd558b6c96ea6593bede5fb40
kathu1207 <wnsdn69@naver.com>
2017-03-09 17:11:53 +0900

    병합정렬

--- 17_03_08(6)/17_03_08(6)/Program.cs
+++ 17_03_08(6)/17_03_08(6)/Program.cs
@@ -6,22 +6,60 @@ using System.Threading.Tasks;
 
 namespace _17_03_08_6_
 {
-    // 분할 하고 분할해서 마지막에 개별로 만든 다음에 큰수 끼리 비교..(토너먼트)
+    // 분할 하고 분할해서 마지막에 개별로 만든 다음에 큰수 끼리 비교..(병합정렬)
     class Program
     {
-        public static void division(int[] arr)// 반을 쪼개는 작업
+        public static void merge(int[] arr,int left,int right,int end,int[] arr2)
         {
-            int[] save = new int[arr.Length];
-            if(save[])
-            for(int i= 0; i<arr.Length; i++)
-            {
+            int i = left, j = right, k = left;
 
+            while(j < end && i < right)
+            {
+                if (arr[i] <= arr[j])
+                    arr2[k++] = arr[i++];
+                else
+                    arr2[k++] = arr[j++];
+            }
+            if(j >= end)
+            {
+                while(i < right)
+                    arr2[k++] = arr[i++];
+            }
+            else if (i >= right)
+            {
+                while(j < end)
+                    arr2[k++] = arr[j++];
+            }
+        }
+        public static void division(int[] arr, int[] arr2)// 반을 쪼개는 작업
+        {
+            for(int i= 1; i<arr.Length; i= 2*i)
+            {
+                for(int j = 0; j <arr.Length-i; j += 2*i )
+                {
+                    int end = j + 2 * i;
+
+                    if (end > arr.Length)
+                        end = arr.Length;
+                    merge(arr, j, j + i, end, arr2);
+                }
+                for (int k = 0; k < arr.Length; ++k)
+                    arr[k] = arr2[k];
             }
         }
         static void Main(string[] args)
         {
             int[] score = new int[20]{
                 80,74,81,90,34,84,76,95,45,66,74,82,76,57,51,88,73,98,51,60};
+
+            int[] score_2 = new int[20];
+
+            division(score, score_2);
+                
+                for (int i = 1; i < score.Length; i++)
+                            Console.WriteLine(score[i]);
+
+
 
         }
     }
