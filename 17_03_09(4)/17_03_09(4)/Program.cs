9183afacc2a21f2f14d8152f11989c67a2651e61
kathu1207 <wnsdn69@naver.com>
2017-03-09 16:19:05 +0900

    히노이의 탑

--- 17_03_09(4)/17_03_09(4)/Program.cs
+++ 17_03_09(4)/17_03_09(4)/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace _17_03_09_4_
 {
+    
+    //히노이의 탑
     class Program
     {
+
+        public static void hanoi(char a, char b, char c,int n)
+        {
+           if(1 == n)
+            {
+                Console.WriteLine("{0} - {1} -> {2}", a, n, c);
+            }
+            else
+            {
+                hanoi(a, c, b, n - 1);
+                Console.WriteLine("{0} - {1} -> {2}", a, n, c);
+                hanoi(b, a, c, n - 1);
+            }
+        }
+
         static void Main(string[] args)
         {
+            hanoi('A', 'B', 'C', 4);
         }
     }
 }
