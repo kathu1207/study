2183ed7ea8ecc1d55fc7be0f67fb104264debbd2
kathu1207 <wnsdn69@naver.com>
2017-03-15 18:16:14 +0900

    공부

---
+++ 17_03_15(2)/17_03_15(2)/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_03_15_2_
+{
+    //원형 큐 
+
+    class MyQueue
+    {
+        int[] QueueArr;
+        int frount = 0;
+        int rear = 0;
+
+        public void enQueue(int a)
+        {
+            if ((rear + 1)% QueueArr.Length == frount)
+            {
+                Console.WriteLine("메모리가 가득 찼습니다.");
+            }
+            else
+            {
+                Console.WriteLine("{0}번째 메모리가 참", rear);
+                QueueArr[rear++] = a;
+                rear = rear % QueueArr.Length;
+            }
+            
+        }
+
+        public int deQueue()
+        {
+            if (rear == frount)
+            {
+                Console.WriteLine("메모리가 비어있습니다.");
+            }
+            else if (frount < QueueArr.Length)
+            {
+                Console.WriteLine("{0}번째 메모리가 뺌", frount);
+                return QueueArr[frount++];
+            }
+            else 
+            {
+                frount = 0;
+            }
+            return 0;
+        }
+
+        public MyQueue(int size)
+        {
+            QueueArr = new int[size];
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyQueue Que = new MyQueue(7);
+        }
+    }
+}
---
+++ 17_03_15/17_03_15/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_03_15
+{
+    //괄호를 확인하는 스택을 만드시오.
+
+    class bracket
+    {
+        int[] intArr;
+        int top = 0;
+
+        public void push(int val)
+        {
+            // 스택에 넣는 과정 
+
+            if (top < intArr.Length)
+            {
+                intArr[top++] = val;
+                Console.WriteLine(top);
+            }
+            else
+                Console.WriteLine("stack이 가득참");
+        }
+
+        public int pop()
+        {
+            // 스택에서 빼는 과정 
+
+            if (top > 0)
+            {
+                top--;
+                return intArr[top];
+            }
+            else
+                Console.WriteLine("더 이상 꺼낼 수 없다.");
+            return 0;
+        }
+
+        public int such()
+        {
+
+        }
+
+        public bracket(int size)
+        {
+            int[] intArr = new int[size];
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bracket numsuch = new bracket(10);
+
+            numsuch.push();
+        }
+    }
+}
