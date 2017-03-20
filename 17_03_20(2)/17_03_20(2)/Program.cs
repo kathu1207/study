1a5d7ca4c0d2299970af31ff4e4cb50e29643bfb
kathu1207 <wnsdn69@naver.com>
2017-03-20 17:16:06 +0900

    tree

---
+++ 17_03_20(2)/17_03_20(2)/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_03_20_2_
+{
+    // 트리를 이용한 계산기
+    class Program
+    {
+        class node
+        {
+            public int  val;
+            public char math;
+            public node left;
+            public node right;
+
+            public node()
+            {
+                val = 0;
+                left = null;
+                right = null;
+            }
+            public node(int _val)
+            {
+                val = _val;
+                left = null;
+                right = null;
+            }
+            public node(char _math)
+            {
+                math = _math;
+                left = null;
+                right = null;
+            }
+        }
+        class tree
+        {
+            node root;
+
+            public void Insert(node parent, node left, node right)
+            {
+                parent.left = left;
+                parent.right = right;
+            }
+
+            void TravelsByPost(node load)
+            {
+                if (load.left != null)
+                    TravelsByPost(load.left);
+
+                if (load.right != null)
+                    TravelsByPost(load.right);
+
+                if ((char.IsSymbol(load.math)) || (char.IsPunctuation(load.math)))
+                {
+                    int a = load.left.val;
+                    int b = load.right.val;
+
+                    switch (load.math)
+                    {
+                        case '*':
+                            load.val = a * b;
+                            break;
+                        case '+':
+                            load.val = a + b;
+                            break;
+                        case '-':
+                            load.val = a - b;
+                            break;
+                        case '/':
+                            load.val = a / b;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                
+               
+                    Console.Write(load.val + " ");
+            }
+
+            public void TravelsByPost()
+
+            {
+                TravelsByPost(root);
+            }
+
+            public void SetRoot(node rootNode)
+            {
+                root = rootNode;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            node tmpNode1 = new node('+');
+            node tmpNode2 = new node(2);
+            node tmpNode3 = new node('*');
+            //node tmpNode4 = new node();
+            //node tmpNode5 = new node();
+            node tmpNode6 = new node(3);
+            node tmpNode7 = new node(7);
+
+            tree Tree = new tree();
+
+            Tree.SetRoot(tmpNode1);
+
+            Tree.Insert(tmpNode1, tmpNode2, tmpNode3);
+            //Tree.Insert(tmpNode2, tmpNode4, tmpNode5);
+            Tree.Insert(tmpNode3, tmpNode6, tmpNode7);
+
+            Tree.TravelsByPost();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 17_03_20/17_03_20/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_03_20
+{
+    // 이진트리(수정 요망)
+    class node
+    {
+        public int val;
+        public node right;
+        public node left;
+    }
+    class preordertrea
+    {
+        node root;
+        node tmp1;
+        node tmp2;
+        node tmp3;
+        node tmp4;
+        node tmp5;
+        node tmp6;
+        node tmp7;
+
+        public preordertrea()
+        {
+            root = null;         
+        }
+
+        public void push(int val)
+        {
+            node tmpnode = new node();
+            tmpnode.val = val;
+            tmpnode.left = null;
+            tmpnode.right = null;
+
+            if (root == null)
+            {
+                tmp1 = tmpnode;
+                root = tmp1;
+            }
+            else if(tmp1.left == null)
+            {
+                tmp2 = tmpnode;
+                tmp1.left = tmp2;
+            }
+            else if(tmp1.right == null)
+            {
+                tmp3 = tmpnode;
+                tmp1.right = tmp3;
+            }
+            else if(tmp2.left == null)
+            {
+                tmp4 = tmpnode;
+                tmp2.left = tmp4;
+            }
+            else if (tmp2.right == null)
+            {
+                tmp5 = tmpnode;
+                tmp2.right = tmp5;
+            }
+            else if (tmp3.left == null)
+            {
+                tmp6 = tmpnode;
+                tmp3.left = tmp6;
+            }
+            else if (tmp3.right == null)
+            {
+                tmp7 = tmpnode;
+                tmp3.right = tmp7;
+            }
+        }
+        public int pop()
+        {
+            if(root != null)
+            {
+                if(tmp1.left != null)
+                {
+                    if (tmp2.left != null)
+                    {
+                        tmp2.left = null;
+                        return tmp4.val;
+                    }
+                    else if (tmp2.right != null)
+                    {
+                        tmp2.right = null;
+                        return tmp5.val;
+                    }
+                    else
+                    {
+                        tmp1.left = null;
+                        return tmp2.val;
+                    }   
+                }
+                else if(tmp1.right != null)
+                {
+                    if (tmp3.left != null)
+                    {
+                        tmp3.left = null;
+                        return tmp6.val;
+                    }
+                    else if (tmp3.right != null)
+                    {
+                        tmp3.right = null;
+                        return tmp7.val;
+                    }
+                    else
+                    {
+                        tmp1.right = null;
+                        return tmp3.val;
+                    }
+                }
+                root = null;
+                return tmp1.val;
+            }
+            return 0;
+        } 
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
