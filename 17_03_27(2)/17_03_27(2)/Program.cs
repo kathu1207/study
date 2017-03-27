7a087b17d7983747b4f6d0abf506b1b7c150b960
kathu1207 <wnsdn69@naver.com>
2017-03-27 17:02:38 +0900

    공부

---
+++ 17_03_27(2)/17_03_27(2)/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_03_27_2_
+{
+    class Program
+    {
+        //BST(Binary search Tree)
+
+        class node
+        {
+            public int val;
+            public node right;
+            public node left;
+            public node parent;
+
+            public node()
+            {
+                val = 0;
+                left = null;
+                right = null;
+                parent = null;
+            }
+            public node(int _val)
+            {
+                val = _val;
+                left = null;
+                right = null;
+                parent = null;
+            }
+        }
+        class BST
+        {
+            node root;
+            node curtnode;
+            node tmp;
+
+            public BST()
+            {
+                root = null;
+                curtnode = new node();
+                tmp = new node();
+            }
+            public void insert(int _val)
+            {
+                curtnode = root;
+
+                if (root == null)
+                {
+                    root = new node();
+                    root.val = _val;
+                }
+
+                for ( ;curtnode != null;)
+                {
+                    if(curtnode.val > _val)
+                    {
+                        if (curtnode.left != null)
+                        {
+                            curtnode = curtnode.left;
+                        }
+                        else
+                        {
+                            curtnode.left = new node(_val);
+                            curtnode.left.parent = curtnode;
+                            break;
+
+                        }
+                    }
+                    else
+                    {
+                        if (curtnode.right != null)
+                        {
+                            curtnode = curtnode.right;
+                        }
+                        else
+                        {
+                            curtnode.right = new node(_val);
+                            curtnode.right.parent = curtnode;
+                            break;
+                        }
+                    }
+                }
+            }
+            public void delete(int _val)
+            {
+                curtnode = root;
+
+                for (; curtnode != null;)
+                {
+                    if(curtnode.val == _val)
+                    {
+                        swap();
+                    }
+                   
+                    if (curtnode.val > _val)
+                    {
+                        curtnode = curtnode.left;
+                    }
+                    else
+                    {
+                        curtnode = curtnode.right;
+                    }
+                }
+            }
+            public void swap()
+            {
+                if(curtnode.left != null)
+                {
+                    tmp = curtnode.left;
+
+                    for (; tmp != null;)
+                    {
+                        if (tmp.right != null)
+                            tmp = tmp.right;
+                        else
+                        {
+                            curtnode.val = tmp.val;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    tmp = curtnode.right;
+
+                    for (; tmp != null;)
+                    {
+                        if (tmp.left != null)
+                            tmp = tmp.left;
+                        else
+                        {
+                            curtnode.val = tmp.val;
+                            break;
+                        }
+                    }
+                }
+                
+            }
+            public void input()
+            {
+                Console.WriteLine(root.left.val);
+                Console.WriteLine(root.left.right.val);
+                Console.WriteLine(root.left.left.val);
+                Console.WriteLine(" ");
+
+                Console.WriteLine(root.right.val);
+                Console.WriteLine(root.right.right.val);
+                Console.WriteLine(root.right.left.val);
+                Console.WriteLine(" ");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            BST test = new BST();
+
+            test.insert(14);
+            test.insert(18);
+            test.insert(15);
+            test.insert(19);
+            test.insert(20);
+            test.insert(11);
+            test.insert(12);
+            test.insert(7);
+            test.insert(8);
+            test.insert(9);
+            test.insert(10);
+            test.insert(6);
+            test.insert(5);
+
+            test.input();
+
+            test.delete(11);
+            test.delete(18);
+
+            test.input();
+            
+
+
+
+        }
+    }
+}
---
+++ 17_03_27/17_03_27/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_03_27
+{
+    class Program
+    {
+        class quicksort
+        {
+            int start;
+            int end;
+            int midindex;
+            int[] arry = { 1, 3, 4, 9, 11, 14, 19, 20, 21, 25 };
+
+            public quicksort()
+            {
+                arry = new int[10];
+                start = 0;
+                end = arry.Length-1;
+                midindex = (start + end)/2;
+            }
+
+            public void distribute(int val)
+            {
+                Console.WriteLine("{0} 처음", midindex);
+
+                if (arry[midindex] == val)
+                {
+                    Console.WriteLine(val);
+                }
+                else if (start == end)
+                {
+                    if (arry[start] == val)
+                    {
+                        Console.WriteLine(val);
+                    }
+                    else
+                    {
+                        Console.WriteLine(start);
+                        Console.WriteLine("없다");
+                    }
+                }
+                else if(val > arry[midindex])
+                {
+                    
+
+                    midindex = (start + end) / 2;
+                    Console.WriteLine("{0} 크다",midindex);
+                    distribute(val);
+                   
+                }
+                else if(val < arry[midindex])
+                {
+                    Console.WriteLine("{0} 작다", midindex);
+                    distribute(val);
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            quicksort quick = new quicksort();
+
+            quick.distribute(20);
+        }
+    }
+}
